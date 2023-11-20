794c1d0bfbaafebfb0b20e9677b2bcc24947e2c8
daniel.mlaszlo@yahoo.com <daniel.mlaszlo@yahoo.com>
2023-11-20 17:36:46 +0100

    uibuild

--- app.js
+++ app.js
@@ -3,20 +3,42 @@ const path = require('path');
 const express = require('express');
 require('express-async-errors');
 const app = express();
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const tokenExtractor = require('./middleware/tokenExtractor');
+// SECURITY
+const helmet = require('helmet');
+const rateLimiter = require('express-rate-limit');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+// ROUTES
 const blogRouter = require('./routes/blogRouter');
 const commentRouter = require('./routes/commentRouter');
 const userRouter = require('./routes/userRouter');
 const authRouter = require('./routes/authRouter');
+// MIDDLEWARE
+const tokenExtractor = require('./middleware/tokenExtractor');
 const errorHandlingMiddleware = require('./middleware/errorHandler');
 const middleware = require('./utils/middlewares');
 
+app.use(
+  rateLimiter({
+    windowMs: 1 * 60 * 1000,
+    max: 100,
+  })
+);
+
+app.use(helmet());
 app.use(cors());
-app.use(express.static('dist'));
+app.use(xss());
+app.use(hpp());
+app.use(mongoSanitize());
+
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(express.static('dist'));
+
 app.use(middleware.requestLogger);
 
 app.use(tokenExtractor);
